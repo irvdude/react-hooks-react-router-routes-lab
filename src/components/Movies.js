656f182205b29482bee656f4c4c81cbf725792f2
Irvin Dinkins <irvdude44@gmail.com>
2023-06-22 22:59:03 -0400

    passed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+          <div key={movie.title}>
+            {movie.title}
+            {movie.time}
+            <ul>
+              <li>{movie.genres}</li>
+            </ul>
+          </div>
+      ))}
+    </div>
+  )
 }
 
 export default Movies;
